d0f867895571b7d660f26d6d8c6cf1a346502874
markcharyk <markcharyk@gmail.com>
2014-02-09 11:37:25 -0800

    first commit

---
+++ GithubTest.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The file is", script
+print "The first variable is:", first
+print "The second variable is:", second
+print "The third variable is:", third
